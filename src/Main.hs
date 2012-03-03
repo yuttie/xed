a1c82c5a25b313980e1472d79314d890f6adeaf4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-03-03 23:17:27 +0900

    Use Editline library to provide an interactive interface.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
 module Main (main) where
 
+import System.Environment
+import System.Console.Editline
+
 
 main :: IO ()
-main = return ()
+main = do
+  progName <- getProgName
+  el <- elInit progName
+  setPrompt el (return "> ")
+  setEditor el Vi
+
+  l <- elGets el
+  case l of
+    Nothing -> return ()
+    Just line -> do
+      putStrLn $ "cmd: " ++ show (init line)
