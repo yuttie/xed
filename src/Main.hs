01a11de23b43cbb494c72bac5c672da939616e29
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-03-05 18:45:24 +0900

    Add a command "chars" restructuring a stream into a char stream.
    The followings are newly defined:
    * [class]    StreamType
    * [instance] StreamType Char
    * [instance] Streamtype String
    * [type]     Stream
    * [function] mapStream
    * [function] encapsulate
    * [function] nestLoop

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 module Main (main) where
 
 import Control.Monad
@@ -20,20 +21,70 @@ parseCommandline l = (cmd, args')
     (cmd:args) = split (== ' ') l
     args' = filter (not . null) args
 
-data Modifier u = Modifier String ([u] -> [u])
+class StreamType a where
+  toString   :: Stream a -> [Char]
+  fromString :: [Char]   -> Stream a
 
-instance Show (Modifier u) where
-  show (Modifier n _) = n
+instance StreamType Char where
+  toString (Stream xs) = xs
+  fromString xs        = Stream xs
 
-applyModifiers :: [String] -> [Modifier String] -> [String]
-applyModifiers ls []                  = ls
-applyModifiers ls (Modifier _ f : ms) = applyModifiers (f ls) ms
+instance StreamType String where
+  toString (Stream xs) = unlines xs
+  fromString xs        = Stream $ lines xs
 
-runPipeline :: (Maybe FilePath, [Modifier String]) -> Either String (IO [String])
+data StreamType u => Stream u = Stream [u]
+
+mapStream :: StreamType u => ([u] -> [u]) -> Stream u -> Stream u
+mapStream f (Stream xs) = Stream $ f xs
+
+data StreamType u => Modifier u = Modifier String (Stream u -> Stream u)
+
+instance StreamType u => Show (Modifier u) where
+  show (Modifier name _) = name
+
+encapsulate :: (StreamType u, StreamType v)
+            => String -> [Modifier v] -> Modifier u
+encapsulate name ms = Modifier (name ++ "(" ++ show ms ++ ")")
+                               (\s -> convert $ applyModifiers (convert s) ms)
+  where
+    convert :: (StreamType a, StreamType b) => Stream a -> Stream b
+    convert = fromString . toString
+
+applyModifiers :: StreamType u => Stream u -> [Modifier u] -> Stream u
+applyModifiers s []                  = s
+applyModifiers s (Modifier _ f : ms) = applyModifiers (f s) ms
+
+runPipeline :: (Maybe FilePath, [Modifier String])
+            -> Either String (IO (Stream String))
 runPipeline (Nothing, _)  = Left "Error: source file is not specified."
 runPipeline (Just fp, ms) = Right $ do
   ls <- liftM lines $ readFile fp
-  return $ applyModifiers ls ms
+  return $ applyModifiers (Stream ls) ms
+
+nestLoop :: StreamType u => String -> [Modifier u] -> InputT IO [Modifier u]
+nestLoop name ms = do
+  l <- getInputLine $ name ++ "> "
+  case l of
+    Nothing -> return ms
+    Just line
+      | cmd == "quit" -> return ms
+      | cmd == "show" -> do
+        liftIO $ putStrLn $ "history: " ++ show ms
+        nestLoop name ms
+      | cmd == "take" -> do
+        let n = read $ head args :: Int
+        let ms' = ms ++ [Modifier ("take " ++ show n) (mapStream $ take n)]
+        nestLoop name ms'
+      | cmd == "drop" -> do
+        let n = read $ head args :: Int
+        let ms' = ms ++ [Modifier ("drop " ++ show n) (mapStream $ drop n)]
+        nestLoop name ms'
+      | otherwise -> do
+        liftIO $ putStrLn $ "Unknown command: " ++ cmd
+        nestLoop name ms
+      where
+        (cmd, args) = parseCommandline line
 
 mainLoop :: (Maybe FilePath, [Modifier String]) -> InputT IO ()
 mainLoop s@(fp, ms) = do
@@ -53,16 +104,24 @@ mainLoop s@(fp, ms) = do
         mainLoop (fp', ms)
       | cmd == "run" -> do
         case runPipeline s of
-          Left  e  -> liftIO $ putStrLn e
-          Right ls -> liftIO $ putStr . unlines =<< ls
+          Left  e   -> liftIO $ putStrLn e
+          Right mls -> liftIO $ do
+            Stream ls <- mls
+            putStr $ unlines ls
         mainLoop s
+      | cmd == "chars" -> do
+        innerPipeline <- nestLoop "chars" [] :: InputT IO [Modifier Char]
+        let ms' = ms ++ [encapsulate "chars" innerPipeline]
+        if null innerPipeline
+          then mainLoop s
+          else mainLoop (fp, ms')
       | cmd == "take" -> do
         let n = read $ head args :: Int
-        let ms' = ms ++ [Modifier ("take " ++ show n) (take n)]
+        let ms' = ms ++ [Modifier ("take " ++ show n) (mapStream $ take n)]
         mainLoop (fp, ms')
       | cmd == "drop" -> do
         let n = read $ head args :: Int
-        let ms' = ms ++ [Modifier ("drop " ++ show n) (drop n)]
+        let ms' = ms ++ [Modifier ("drop " ++ show n) (mapStream $ drop n)]
         mainLoop (fp, ms')
       | otherwise -> do
         liftIO $ putStrLn $ "Unknown command: " ++ cmd
